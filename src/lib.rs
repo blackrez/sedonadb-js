7d7594a0b708d7d31d659e309d0268b5c652ea97
Nabil blackrez Servais <nabil@gnop.dev>
2026-03-27 19:47:19 +0100

    first test and it's working

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+#![deny(clippy::all)]
+
+use datafusion::prelude::col;
+use napi_derive::napi;
+use napi::{Error, Result}; // Import napi error types
+use sedona::context::{SedonaContext, SedonaDataFrame};
+
+#[napi]
+pub async fn demo(_sql: String) -> Result<String> {
+    // Convert any DataFusionError to napi::Error
+    let ctx = SedonaContext::new_local_interactive()
+        .await
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+
+    let url = "https://raw.githubusercontent.com/geoarrow/geoarrow-data/v0.2.0/natural-earth/files/natural-earth_cities_geo.parquet";
+    let df = ctx
+        .read_parquet(url, Default::default())
+        .await
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+
+    let output = df
+        .sort_by(vec![col("name")])
+        .map_err(|e| Error::from_reason(e.to_string()))?
+        .show_sedona(&ctx, Some(5), Default::default())
+        .await
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+
+    // Remove unnecessary parentheses
+    Ok(output.into())
+}
